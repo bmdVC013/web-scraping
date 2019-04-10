64ec4289bb1152a4eeb347814a1047ddf6754988
Minh Dung <minhdung.bui13@gmail.com>
2019-04-10 23:26:50 +0700

    sentdex p1 and sort by document

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Web Scraping code practice in Python
 
 ## Preferences:
 + [Automate the Boring Stuff with Python](https://automatetheboringstuff.com/chapter11/) - Al Sweigart
++ [Web scraping and parsing with Beautiful Soup & Python Introduction](https://www.youtube.com/watch?v=aIPqt-OdmS0&list=PLQVvvaa0QuDfV1MIRBOcqClP6VZXsvyZS&index=1) - Sentdex
++ [Beautiful Soup Documentation](https://www.crummy.com/software/BeautifulSoup/bs4/doc/)
---
+++ sentdex_channel/bs4_tut1.py
@@ -0,0 +1,38 @@
+#! python3
+import bs4 as bs 
+import urllib.request
+
+# Grab source code.
+sauce = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
+# Create a BeautifulSoup object.
+soup = bs.BeautifulSoup(sauce,'lxml')
+
+# Title of the page.
+print(soup.title)
+
+# Get attribute.
+print(soup.title.name)
+
+# Get values.
+print(soup.title.string)
+
+# Beginning navigation.
+print(soup.title.parent.name)
+
+# Getting specific value.
+print(soup.p)
+
+# Grab text.
+print(soup.get_text())
+
+# Finding all paragraph tags <p>.
+print(soup.find_all('p'))
+# Also iterate through them
+for paragraph in soup.find_all('p'):
+	print(paragraph.string)		# .string produces a NavigableString object
+	print(str(paragraph.text))	# .text produces unicode text
+
+# To grab links.
+for url in soup.find_all('a'):
+	print(url.get('href'))
+
